06ecd52e0725e20cc9334e3b2368d2da2ef76201
Muhammad <abdullahbinaslam64@gmail.com>
2024-04-15 21:15:49 +0500

    JS Events Start

---
+++ scrip.js
@@ -0,0 +1,75 @@
+
+
+//MAKE TABLE USING FUNCTION                                         
+// function table(tableof,tablelength){
+
+//     for(var i=1;i <= tablelength;i++){
+//              var tabel = tableof + " x " + i + " = " + tableof * i;
+//             //  document.write(tabel)
+//         }
+//         return tabel
+// }
+// console.log(table(+prompt("Enter Table Number"),+prompt("Enter Table Length")));
+
+
+
+//MAKE CALCULATOR USING SWITCH AND CASE
+// function calc(num1, opr, num2) {
+//     switch (opr) {
+
+//         case "+":
+//             return num1 + num2;
+
+//         case "-":
+//             return num1 - num2;
+
+//         case "*":
+//             return num1 * num2;
+
+//         case "/":
+//             return num1 / num2;
+
+//         default:
+//             return "invalid operator";
+//     }
+// }
+// console.log(calc(2, "+", 8));
+// console.log(calc(15, "-", 10));
+// console.log(calc(7, "*", 3));
+// console.log(calc(27, "/", 3));
+
+
+
+                                                       //JS-EVENT LINK
+
+// function myname(){
+//     alert("My Name IS: ABDULLAH BIN ASLAM")
+// }
+
+                                                    //onmouseover and onmouseout
+
+// function over(){
+//    alert("onmouseover")
+// }
+
+// function out(){
+//     alert("onmouseout")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
